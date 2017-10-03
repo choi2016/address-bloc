bf3d95be80e0e3894ea85753cfbb7b2b1cf3b81c
Edward Choi <edwardchoi.2011@gmail.com>
2017-10-02 19:38:20 -0700

    CP6 assignment

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -84,6 +84,23 @@ RSpec.describe AddressBook do
 
 			check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
 		end						
+
+	end
+
+	describe "#import_from_csv_2" do
+		it "imports the correct number of entries from entries_2.csv" do
+			book.import_from_csv("entries_2.csv")
+			book_size = book.entries.size
+
+			expect(book_size).to eq 1
+		end
+		
+		it 'imports the 1st entry for entries_2.csv' do
+			book.import_from_csv('entries_2.csv')
+			entry_one = book.entries[0]
+
+			check_entry(entry_one, "a", "b", "c@d")
+		end
 	end
 end
 
